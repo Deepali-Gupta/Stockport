6ca6d64738b02d89d8406018becc73357718da08
harshdeep gupta <harshdeep gupta>
2017-03-12 13:50:50 +0530

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+var db = require('../queries');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  // res.send('index.html')
+});
+
+router.get('/users', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send('dick');
+  next();
+});
+
+
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+// router.put('/api/puppies/:id', db.updatePuppy);
+// router.delete('/api/puppies/:id', db.removePuppy);
+
+module.exports = router;
